eef1d1c4b1b30cedff85cc89c9603a417c51b510
Sarah Alafif <sar1997yah@gmail.com>
2019-12-11 17:21:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solution in this file!
  const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
-const returnFirstTwoDrivers
+const returnFirstTwoDrivers=function(drivers){
+  
+  
+  
+}
  
  
  
