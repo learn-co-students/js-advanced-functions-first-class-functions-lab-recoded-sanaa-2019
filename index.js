33469533e2bdb0199a4df74359a716708c8dadc1
Sarah Alafif <sar1997yah@gmail.com>
2019-12-11 17:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
  const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
- let [answer, theBase] = (
+ let [first, last] = (
   function(thingToAdd) {
     let base = 3
     return [
