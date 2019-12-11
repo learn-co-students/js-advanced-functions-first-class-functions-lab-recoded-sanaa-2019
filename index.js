20d9947e2f7ca05defd24d295a6c6dccf751f0ff
Sarah Alafif <sar1997yah@gmail.com>
2019-12-11 17:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+ const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+ function returnFirstTwoDrivers(drivers){
+  let pos=0
+   let  iterator = drivers.slice(pos,2); 
+   return iterator
+ }
+ function returnLastTwoDrivers(drivers){
+   let pos=drivers.length-2
+     let  iterator = drivers.slice(pos,4); 
+   return iterator
+ }
+ function selectingDrivers(){
+   
+ }
